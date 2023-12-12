59554df457bc065ed4860c6cdd38bbe0277f1c09
Botond Balogh <balogh.botond8@gmail.com>
2023-12-12 23:04:47 +0100

    Separate screen by authentication

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { auth } from './firebase';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
 import Home from './src/screens/Home';
@@ -9,18 +10,33 @@ import Home from './src/screens/Home';
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setIsAuthenticated(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signup" component={Signup} />
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
-    </SafeAreaProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <NavigationContainer>
+            {isAuthenticated ? (
+              <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+              </Stack.Navigator>
+            ) : (
+              <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Signup" component={Signup} />
+              </Stack.Navigator>
+            )}
+          </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
   );
 }
 
