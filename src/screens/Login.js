82f3fc77c7e6481564f9ae7dc740872420c1ec15
Botond Balogh <balogh.botond8@gmail.com>
2023-12-12 22:38:43 +0100

    Login logic

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../styles/GlobalStyles';
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const navigation = useNavigation();
 
-    const handleSignUpPress = () => {
-      navigation.navigate('Signup');
-    };
-  
     const handleLoginPress = () => {
-      // TODO:Perform authentication...
-      navigation.navigate('Home');
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Successful login
+                console.log('Logged in:', userCredential.user);
+                navigation.navigate('Home');
+            })
+            .catch((error) => {
+                // Login failed
+                Alert.alert('Authentication Failed', error.message);
+            });
+    };
+
+    const handleSignUpPress = () => {
+        navigation.navigate('Signup');
     };
 
     return (
         <SafeAreaView style={GlobalStyles.screenContainer}>
-        <TextInput style={GlobalStyles.inputField} placeholder="Email" />
-        <TextInput style={GlobalStyles.inputField} placeholder="Password" secureTextEntry />
-        <TouchableOpacity style={GlobalStyles.button} onPress={handleLoginPress}>
-            <Text style={GlobalStyles.buttonText}>Log In</Text>
-        </TouchableOpacity>
-        <Text style={GlobalStyles.linkText} onPress={handleSignUpPress}>Sign Up</Text>
+            <TextInput 
+                style={GlobalStyles.inputField} 
+                placeholder="Email" 
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <TextInput 
+                style={GlobalStyles.inputField} 
+                placeholder="Password" 
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry 
+            />
+            <TouchableOpacity style={GlobalStyles.button} onPress={handleLoginPress}>
+                <Text style={GlobalStyles.buttonText}>Log In</Text>
+            </TouchableOpacity>
+            <Text style={GlobalStyles.linkText} onPress={handleSignUpPress}>Sign Up</Text>
         </SafeAreaView>
     );
 }
