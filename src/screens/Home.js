c7d3a84c7aac12f4fdcba3ad1dee9a73e8137b99
Botond Balogh <balogh.botond8@gmail.com>
2023-12-12 22:33:52 +0100

    Add Logout to home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../styles/GlobalStyles';
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
 
 export default function Home() {
-  return (
-    <SafeAreaView style={GlobalStyles.screenContainer}>
-      <Text>Welcome to the Home Screen!</Text>
-      <TouchableOpacity style={GlobalStyles.button}>
-        <Text style={GlobalStyles.buttonText}>Sign Out</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+    const navigation = useNavigation();
+
+    const handleLogout = () => {
+      signOut(auth).then(() => {
+        navigation.navigate('Login');
+      }).catch((error) => {
+        Alert.alert('Logout Failed', error.message);
+      });
+    };
+    return (
+      <SafeAreaView style={GlobalStyles.screenContainer}>
+        <Text>Welcome to the Home Screen!</Text>
+        <TouchableOpacity style={GlobalStyles.button} onPress={handleLogout}>
+          <Text style={GlobalStyles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
 }
