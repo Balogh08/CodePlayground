acbdd3b87322c671f0741420ac5436877f91c000
Botond Balogh <balogh.botond8@gmail.com>
2023-12-12 22:43:54 +0100

    Add form to login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Formik } from 'formik';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import { auth } from '../../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import * as yup from 'yup';
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const navigation = useNavigation();
 
-    const handleLoginPress = () => {
-        signInWithEmailAndPassword(auth, email, password)
+    const handleLoginPress = (values) => {
+        signInWithEmailAndPassword(auth, values.email, values.password)
             .then((userCredential) => {
-                // Successful login
                 console.log('Logged in:', userCredential.user);
                 navigation.navigate('Home');
             })
             .catch((error) => {
-                // Login failed
                 Alert.alert('Authentication Failed', error.message);
             });
     };
@@ -28,26 +31,42 @@ export default function Login() {
     };
 
     return (
-        <SafeAreaView style={GlobalStyles.screenContainer}>
-            <TextInput 
-                style={GlobalStyles.inputField} 
-                placeholder="Email" 
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <TextInput 
-                style={GlobalStyles.inputField} 
-                placeholder="Password" 
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry 
-            />
-            <TouchableOpacity style={GlobalStyles.button} onPress={handleLoginPress}>
-                <Text style={GlobalStyles.buttonText}>Log In</Text>
-            </TouchableOpacity>
+        <View style={GlobalStyles.screenContainer}>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={handleLoginPress}
+                validationSchema={loginSchema}
+            >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                    <>
+                        <TextInput 
+                            style={GlobalStyles.inputField} 
+                            placeholder="Email" 
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                        />
+                        {touched.email && errors.email && <Text>{errors.email}</Text>}
+
+                        <TextInput 
+                            style={GlobalStyles.inputField} 
+                            placeholder="Password" 
+                            onChangeText={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password}
+                            secureTextEntry 
+                        />
+                        {touched.password && errors.password && <Text>{errors.password}</Text>}
+
+                        <TouchableOpacity style={GlobalStyles.button} onPress={handleSubmit}>
+                            <Text style={GlobalStyles.buttonText}>Log In</Text>
+                        </TouchableOpacity>
+                    </>
+                )}
+            </Formik>
             <Text style={GlobalStyles.linkText} onPress={handleSignUpPress}>Sign Up</Text>
-        </SafeAreaView>
+        </View>
     );
 }
