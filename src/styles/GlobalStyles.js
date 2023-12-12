1f82d28eb203348ac1a0024ab0b327973fa9db84
Botond Balogh <balogh.botond8@gmail.com>
2023-12-12 20:08:13 +0100

    Create GlobalStyles.js

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export const GlobalStyles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 12,
+  },
+  inputField: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '100%',
+    marginBottom: 15,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 15,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  linkText: {
+    color: 'blue',
+    textAlign: 'center',
+    marginTop: 15,
+  },
+});
