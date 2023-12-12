8a4ae22c2e50532569e633598ab5f5be645962bf
Botond Balogh <balogh.botond8@gmail.com>
2023-12-12 20:33:24 +0100

    Add simple stack navigation

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../styles/GlobalStyles';
 
 export default function LoginScreen() {
-  return (
-    <SafeAreaView style={GlobalStyles.screenContainer}>
-      <TextInput style={GlobalStyles.inputField} placeholder="Email" />
-      <TextInput style={GlobalStyles.inputField} placeholder="Password" secureTextEntry />
-      <TouchableOpacity style={GlobalStyles.button}>
-        <Text style={GlobalStyles.buttonText}>Log In</Text>
-      </TouchableOpacity>
-      <Text style={GlobalStyles.linkText}>Sign Up</Text>
-    </SafeAreaView>
-  );
+    const navigation = useNavigation();
+
+    const handleSignUpPress = () => {
+      navigation.navigate('Signup');
+    };
+  
+    const handleLoginPress = () => {
+      // TODO:Perform authentication...
+      navigation.navigate('Home');
+    };
+
+    return (
+        <SafeAreaView style={GlobalStyles.screenContainer}>
+        <TextInput style={GlobalStyles.inputField} placeholder="Email" />
+        <TextInput style={GlobalStyles.inputField} placeholder="Password" secureTextEntry />
+        <TouchableOpacity style={GlobalStyles.button} onPress={handleLoginPress}>
+            <Text style={GlobalStyles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <Text style={GlobalStyles.linkText} onPress={handleSignUpPress}>Sign Up</Text>
+        </SafeAreaView>
+    );
 }
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../styles/GlobalStyles';
 
-export default function SignupScreen() {
-  return (
-    <SafeAreaView style={GlobalStyles.screenContainer}>
-      <TextInput style={GlobalStyles.inputField} placeholder="Email" />
-      <TextInput style={GlobalStyles.inputField} placeholder="Password" secureTextEntry />
-      <TextInput style={GlobalStyles.inputField} placeholder="Re-enter Password" secureTextEntry />
-      <TouchableOpacity style={GlobalStyles.button}>
-        <Text style={GlobalStyles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
-      <Text style={GlobalStyles.linkText}>Cancel</Text>
-    </SafeAreaView>
-  );
+export default function SignupScreen() {  
+    const navigation = useNavigation();
+
+    const handleCancelPress = () => {
+      navigation.goBack();
+    };
+
+    return (
+        <SafeAreaView style={GlobalStyles.screenContainer}>
+        <TextInput style={GlobalStyles.inputField} placeholder="Email" />
+        <TextInput style={GlobalStyles.inputField} placeholder="Password" secureTextEntry />
+        <TextInput style={GlobalStyles.inputField} placeholder="Re-enter Password" secureTextEntry />
+        <TouchableOpacity style={GlobalStyles.button}>
+            <Text style={GlobalStyles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+        <Text style={GlobalStyles.linkText} onPress={handleCancelPress}>Cancel</Text>
+        </SafeAreaView>
+    );
 }
