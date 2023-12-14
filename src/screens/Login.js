0456fe90600a24844a2c36af5d5d02fb07a98de6
Balogh Botond <44845895+Balogh08@users.noreply.github.com>
2023-12-14 19:21:54 +0100

    Email/password auth setup (#4)
    * install firebase
    
    * Hello world app
    
    * app jsx instead of tsx
    
    * Create GlobalStyles.js
    
    * Add Home,Login and Signup screens
    
    * Update SignupScreen.js
    
    * Adding simple screens to app.jsx
    
    * Intall @react-navigation/native, react-native-safe-area-context, react-native-screens
    
    * Add simple stack navigation
    
    * Rename the screens
    
    * install formik yup
    
    * Implement form, validation and signup
    
    * Add Logout to home screen
    
    * Login logic
    
    * Add form to login screen
    
    * Refactoring signup
    
    * Show logged in user email address
    
    * Separate screen by authentication
    
    * Update gitignore
    
    * move keys to .env
    
    * Create firebase.js

---
+++ src/screens/Login.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Formik } from 'formik';
+import { GlobalStyles } from '../styles/GlobalStyles';
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import * as yup from 'yup';
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
+
+export default function Login() {
+    const navigation = useNavigation();
+
+    const handleLoginPress = (values) => {
+        signInWithEmailAndPassword(auth, values.email, values.password)
+            .then((userCredential) => {
+                console.log('Logged in:', userCredential.user);
+                navigation.navigate('Home');
+            })
+            .catch((error) => {
+                Alert.alert('Authentication Failed', error.message);
+            });
+    };
+
+    const handleSignUpPress = () => {
+        navigation.navigate('Signup');
+    };
+
+    return (
+        <View style={GlobalStyles.screenContainer}>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={handleLoginPress}
+                validationSchema={loginSchema}
+            >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                    <>
+                        <TextInput 
+                            style={GlobalStyles.inputField} 
+                            placeholder="Email" 
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                        />
+                        {touched.email && errors.email && <Text>{errors.email}</Text>}
+
+                        <TextInput 
+                            style={GlobalStyles.inputField} 
+                            placeholder="Password" 
+                            onChangeText={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password}
+                            secureTextEntry 
+                        />
+                        {touched.password && errors.password && <Text>{errors.password}</Text>}
+
+                        <TouchableOpacity style={GlobalStyles.button} onPress={handleSubmit}>
+                            <Text style={GlobalStyles.buttonText}>Log In</Text>
+                        </TouchableOpacity>
+                    </>
+                )}
+            </Formik>
+            <Text style={GlobalStyles.linkText} onPress={handleSignUpPress}>Sign Up</Text>
+        </View>
+    );
+}
