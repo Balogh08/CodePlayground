7f7832e6d6edd9050aa12f51dfca153dd8c7d9e6
Botond Balogh <balogh.botond8@gmail.com>
2024-01-10 11:52:26 +0100

    FB login on Android works
    User is created in firebase too

--- src/components/FacebookLoginButton.js
+++ src/components/FacebookLoginButton.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { LoginButton, AccessToken } from 'react-native-fbsdk-next';
-import { auth, facebookProvider } from '../../firebase';
-import { signInWithCredential } from 'firebase/auth';
+import { LoginButton, LoginManager, AccessToken } from 'react-native-fbsdk-next';
+import { auth } from '../../firebase';
+import { signInWithCredential, FacebookAuthProvider } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 
 const FacebookLoginButton = () => {
   const navigation = useNavigation();
 
-  const handleFacebookLogin = async (error, result) => {
-    console.log('Start handleFacebookLogin');
-    if (error) {
-      console.log("login has error: " + result.error);
-    } else if (result.isCancelled) {
-      console.log("login is cancelled.");
-    } else {
-      console.log('1 else');
+  const handleFacebookLogin = async () => {
+    try {
+      const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+      if (result.isCancelled) {
+        console.log("User cancelled the Facebook login.");
+        return;
+      }
+
+      console.log("Facebook login was successful!");
+
       const data = await AccessToken.getCurrentAccessToken();
       if (!data) {
-        console.log('Something went wrong obtaining access token');
-      } else {
-        console.log('data: ', data);
-        console.log('facebookProvider: ', facebookProvider);
-        const facebookCredential = facebookProvider.credential(data.accessToken);
-        console.log('facebookCredential: ', facebookCredential);
-        await signInWithCredential(auth, facebookCredential)
-          .then(() => {
-            console.log('Navigating home');
-            navigation.navigate('Home');
-          })
-          .catch((error) => {
-            console.log('Authentication failed:', error.message);
-          });
-          console.log('NO home');
+        console.log('Something went wrong obtaining the Facebook access token');
+        return;
       }
+
+      const facebookCredential = FacebookAuthProvider.credential(data.accessToken);
+      const userCredential = await signInWithCredential(auth, facebookCredential);
+      
+      console.log('User signed in with Facebook!', userCredential.user);
+      navigation.navigate('Home');
+    } catch (error) {
+      console.log('Facebook login or Firebase credential failed:', error);
     }
   };
 
