c6c16d588fc7bc96f98901ee97ad9daa249b6829
Botond Balogh <balogh.botond8@gmail.com>
2024-01-19 19:45:50 +0100

    Configuring podfile, appdelegate and rest
    set team in xcode 'signing & capabilities'
    
    code is crashing because missing ids and tokens from .env
    
    source:
    https://rnfirebase.io/auth/social-auth
    https://www.youtube.com/watch?v=SAWb7_v6zYg

--- ios/CodePlayground/AppDelegate.mm
+++ ios/CodePlayground/AppDelegate.mm
@@ -2,10 +2,19 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
+#import <AuthenticationServices/AuthenticationServices.h>
+#import <SafariServices/SafariServices.h>
+#import <FBSDKCoreKit/FBSDKCoreKit-Swift.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                  didFinishLaunchingWithOptions:launchOptions];
+
   self.moduleName = @"CodePlayground";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
