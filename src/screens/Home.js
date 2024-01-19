b6cba2c0a99d5642b4749ae590d13caaad258cbb
Botond Balogh <balogh.botond8@gmail.com>
2024-01-19 19:33:52 +0100

    Logout by fb too

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,7 @@ import { SafeAreaView, Text, TouchableOpacity, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import auth from '@react-native-firebase/auth';
+import { LoginManager } from 'react-native-fbsdk-next';
 
 export default function Home() {
     const [userEmail, setUserEmail] = useState('');
@@ -18,7 +19,10 @@ export default function Home() {
     }
 
     const handleLogout = () => {
-      auth().signOut().then().catch((error) => {
+      auth().signOut().then(() => {
+        // Log out from Facebook
+        LoginManager.logOut();
+      }).catch((error) => {
         Alert.alert('Logout Failed', error.message);
       });
     };
